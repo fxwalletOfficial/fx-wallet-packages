b2bdad38e8a9403b2ee080a3108bd37ed5db6aef
pzhun <1414807496@qq.com>
2025-09-11 11:34:50 +0800

    update: add record demo.

--- packages/aleo_dart/test/wss_upgrade_demo.dart
+++ packages/aleo_dart/test/wss_upgrade_demo.dart
@@ -24,6 +24,10 @@ Future<void> main() async {
       'record1qyqsq8nktvwx2tq3ys4mmfclqexs89xv9uvstx7e5ndkevk4ayfqefsqqyxx66trwfhkxun9v35hguerqqpqzqqyuvhywm8dwg3c4l7t2pzd9mcv52ga54a8cns034vcncc2mf7upycqj68vqmwhvze0wc8yr3khunjnhszlyaavypvgy7wwlqsala9syctqjej';
 
   // 解析record
+  // record1qyqsq8nktvwx2tq3ys4mmfclqexs89xv9uvstx7e5ndkevk4ayfqefsqqyxx66trwfhkxun9v35hguerqqpqzqqyuvhywm8dwg3c4l7t2pzd9mcv52ga54a8cns034vcncc2mf7upycqj68vqmwhvze0wc8yr3khunjnhszlyaavypvgy7wwlqsala9syctqjej
+  // 685640431604231063444970805268472233140731435211848949355942532669597254133field
+  // at1zqryps9nrxpq2d7ryj09x3yyg09xkjag0rhcsyjt87f5chzljgrqztg5vg
+  
 
   final recordData = rustRecordLib.decryptCipherText(publicRecord, view_key);
 
