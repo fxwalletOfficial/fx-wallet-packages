e7b4b6f92b253c066aa31b67bdf7ffe4cb82cc89
xubo <1308425295@qq.com>
2025-09-26 18:12:48 +0800

    feat: add cow swap transaction abi data parse

--- packages/crypto_wallet_util/lib/src/transaction/eth/lib/eth_data_decoder.dart
+++ packages/crypto_wallet_util/lib/src/transaction/eth/lib/eth_data_decoder.dart
@@ -1,12 +1,10 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:crypto_wallet_util/src/transaction/eth/lib/typed_data/abi_decoder.dart';
+import 'package:crypto_wallet_util/src/transaction/eth/lib/abi/cowswap_abi.dart';
+import 'package:crypto_wallet_util/src/transaction/eth/lib/abi/paraswap_abi.dart';
 
 class EthDataDecoder {
-  static final AbiDecoder paraSwap = AbiDecoder.fromABI(json.decode(
-      File('./lib/src/transaction/eth/lib/abi/paraswap_abi.json')
-          .readAsStringSync(encoding: utf8)));
+  static final AbiDecoder paraSwap = AbiDecoder.fromABI(paraSwapAbi);
+  static final AbiDecoder cowSwap = AbiDecoder.fromABI(cowswapAbi);
 
   static decodeByAbi(List<dynamic> abiJson, String data) {
     final decoder = AbiDecoder.fromABI(abiJson);
